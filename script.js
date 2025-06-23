d99f7c034691a4af0384caf826da32ed0e0b0a7c
That-weird-kid <zacxh.messi@gmail.com>
2025-06-23 15:59:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@
     target.textContent = '🏳️‍🌈 Happy Pride Month! 🏳️‍🌈'
   });
   observer.observe(target, { childList: true, characterData: true, subtree: true });
+const target = document.getElementById('pride');
+
+const observer = new MutationObserver(() => {
+  observer.disconnect(); // 🔴 Stop watching
+
+  alert("I know you might not like pride month but I do, and you can be patient, it will be gone at the end of June.");
+  target.textContent = '🏳️‍🌈 Happy Pride Month! 🏳️‍🌈';
+
+  observer.observe(target, {  // 🟢 Start watching again
+    childList: true,
+    characterData: true,
+    subtree: true
+  });
+});
+
+observer.observe(target, {
+  childList: true,
+  characterData: true,
+  subtree: true
+});
+
